bf17c17ca1fb71d7e000218e6b269d4258ca94ab
prasenjit1113 <2001prasenjitpaul@gmail.com>
2025-09-05 16:33:55 +0530

    some changes

---
+++ dev2.O/js-hindi-youtube-main/01_basics/test.js
@@ -0,0 +1,6 @@
+let myDate = new Date();
+console.log(myDate.toDateString())
+console.log(myDate.getTime())
+console.log(myDate.toISOString())
+console.log(myDate.toLocaleString())
+console.log(typeof myDate)
