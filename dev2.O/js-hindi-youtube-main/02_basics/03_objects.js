0cc3f82a47a6d40f79f3b08a28076808a20e5a42
prasenjit1113 <2001prasenjitpaul@gmail.com>
2025-09-07 14:31:57 +0530

    lets start

---
+++ dev2.O/js-hindi-youtube-main/02_basics/03_objects.js
@@ -0,0 +1,43 @@
+//objects are created using 2 methods
+
+// singleton method
+// Object.create
+// Object.create() Method: This method creates a new object using an existing object as its prototype, enabling inheritance. 
+
+
+
+//non-singleton method
+// object literals
+
+const mySym = Symbol("key1")  //declare a symbol data type
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser.email);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+JsUser.greeting();
+JsUser.greetingTwo();
